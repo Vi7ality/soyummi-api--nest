32128b079a5d0e45cfdc69c518c3d8082079fee9
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-03-04 00:09:04 +0200

    findAll recipe service added

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Recipe, RecipeDocument } from './schemas/recipe.schema';
 
 @Injectable()
-export class RecipesService {}
+export class RecipesService {
+  constructor(
+    @InjectModel(Recipe.name)
+    private readonly recipeModel: Model<RecipeDocument>,
+  ) {}
+
+  async findAll(): Promise<Recipe[]> {
+    const recipes = await this.recipeModel.find().exec();
+    return recipes;
+  }
+}
