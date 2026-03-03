7fd88305340869431795770f073d4c8061bd79e4
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-03-03 23:52:01 +0200

    recipe controller main endpoints added

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,8 +1,66 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { RecipesService } from './recipes.service';
 
 @Controller('recipes')
 export class RecipesController {
-    constructor(private)
-    @Get()
+  constructor(private recipeService: RecipesService) {}
 
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get('popular-recipe')
+  findPopular() {
+    return [];
+  }
+
+  @Get('popular-recipe/:category')
+  findPopularByCategory() {
+    return [];
+  }
+
+  @Get('own-recipes')
+  findOwnRecipes() {
+    return [];
+  }
+
+  @Post('own-recipes')
+  addOwn() {
+    return [];
+  }
+
+  @Delete('own-recipe/:id')
+  deleteOwn() {
+    return [];
+  }
+
+  @Get('main-page')
+  findMainPageRecipes() {
+    return [];
+  }
+  @Get('list/:category')
+  findListByCategory() {
+    return [];
+  }
+
+  @Get('category-list')
+  findCategoryList() {
+    return [];
+  }
+
+  @Get('/search')
+  searchRecipe() {
+    return [];
+  }
+
+  @Get('/ingredients')
+  findIngredients() {
+    return [];
+  }
+
+  @Get(':id')
+  findIdRecipe(@Param('id') id: string) {
+    return id;
+  }
 }
